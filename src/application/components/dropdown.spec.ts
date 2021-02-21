18c9033487388ea64e676bb9b316ac8456910718
Andre Marques <marques.andre.lm@gmail.com>
2021-02-21 21:15:49 +0000

    Added jest

---
+++ src/application/components/dropdown.spec.ts
@@ -0,0 +1,13 @@
+describe('Dropdown Component', () => {
+    it('should return 2 when 1 + 1', () => {
+        // Given
+        const a = 1;
+        const b = 1;
+
+        // When
+        const result = a + b;
+
+        // Then
+        expect(result).toBe(2);
+    });
+});
