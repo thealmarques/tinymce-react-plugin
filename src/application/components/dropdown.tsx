835e54bc1ec8d045b926a73e9dbdaf20011c8f4e
Andre Marques <marques.andre.lm@gmail.com>
2021-08-27 17:09:57 +0100

    Finished basic template

--- src/application/components/dropdown.tsx
+++ src/application/components/dropdown.tsx
@@ -2,10 +2,17 @@ import * as React from 'react';
 import './dropdown.scss';
 
 export const Dropdown = () => {
+    const [isOpen, setIsOpen] = React.useState<boolean>(false);
+
     return (
-        <ul className='sacode'>
-            <li onClick={() => console.log('clicked!')}>Ai</li>
-            <li>ui</li>
-        </ul>
+        <div className='dropdown'>
+            <span onClick={() => setIsOpen(!isOpen)} className={'dropdown__base'}>Click me</span>
+            { isOpen && (
+                <ul className='dropdown__options'>
+                    <li>Option 1</li>
+                    <li>Option 2</li>
+                </ul>
+            )}
+        </div>
     );
 };
