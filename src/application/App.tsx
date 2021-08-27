b8aab93dc2055e415487f6096545e622af8d09f4
Andre Marques <marques.andre.lm@gmail.com>
2021-08-27 15:40:38 +0100

    another ez one

--- src/application/App.tsx
+++ src/application/App.tsx
@@ -3,12 +3,8 @@ import { render } from 'react-dom';
 import { Dropdown } from './components/dropdown';
 
 export const setupReactApp = (element: Element) => {
-    const App = () => (
-        <Dropdown />
-    );
-
     render(
-        <App />,
+        <Dropdown />,
         element.insertAdjacentElement('beforeend', document.createElement('div')),
     );
 };
